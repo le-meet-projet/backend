df0935605116455dfbe49d69eb846da87ec07eae
Youssef El Gharib <youssef.elgharib17@gmail.com>
2020-10-21 17:20:23 +0100

    Add controllers functions

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class UserApiController extends Controller
 {
-    //
+    /**
+     * VALIDATE REQUEST
+     * CREATE
+     */
+    public function register()
+    {
+        return null;
+    }
+
+    /**
+     * VALIDATE REQUEST
+     * UPDATE
+     */
+    public function updateInfo()
+    {
+        return null;
+    }
+    /**
+     * CHECK THE PASSWORDS ARE UNIQUE
+     * LOGIN
+     */
+    public function login()
+    {
+        return null;
+    }
 }
