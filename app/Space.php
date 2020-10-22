144f9a3e0fdd433319428195bd4bfe9fe8f9f595
Youssef El Gharib <youssef.elgharib17@gmail.com>
2020-10-22 18:35:59 +0100

    Fix errors git hub

--- app/Space.php
+++ app/Space.php
@@ -3,20 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-<<<<<<< HEAD
-
-class Space extends Model
-{
-    //
-=======
- 
-
- 
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Space extends Model
 {
     use SoftDeletes;
- 
->>>>>>> 5ff9e506ed96118b2e61bf6bdd923ef1a91c1047
 }
