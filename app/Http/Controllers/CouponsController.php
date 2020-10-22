01162894bd4522a5676180b6b7476a8a0cded1ed
Youssef El Gharib <youssef.elgharib17@gmail.com>
2020-10-22 13:24:42 +0100

    add models and create CouponsController functions

--- app/Http/Controllers/CouponsController.php
+++ app/Http/Controllers/CouponsController.php
@@ -2,27 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use App\Coupon;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Validation\ValidationException;
+use Illuminate\View\View;
 
 class CouponsController extends Controller
 {
+
     public function index()
     {
-        $media = Media::orderby('id', 'desc')->paginate(10);
-        return view('admin.media.index', compact('media'));
+        $coupons = Coupon::orderby('id', 'desc')->paginate(10);
+        return view('admin.Coupon.index', compact('coupons'));
     }
 
+    /**
+     * @return RedirectResponse
+     */
     public function bulkdelete()
     {
         Media::truncate();
         return redirect()->route('admin.media.home')->with('success', 'data has been deleted successfully');
     }
 
-    // Store coupon record
+    /**
+     * Create new coupon record
+     *
+     * @return View
+     */
+    public function create()
+    {
+        return view('admin.coupons.create');
+    }
+
+    /**
+     * Edit coupon record
+     *
+     * @param $id
+     * @return View
+     */
+    public function edit(int $id)
+    {
+        $content = Coupon::whereId($id)->first();
+        return view('admin.coupons.edit', compact('content'));
+    }
+
+
+    /**
+     * Store coupon record
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     * @throws ValidationException
+     */
     public function store(Request $request)
     {
         $this->validate($request, [
-            'coupon' => 'required|unique:coupons',
+            'code' => 'required|unique:coupon',
             'discount' => 'required|max:3'
         ]);
 
@@ -32,12 +70,11 @@ class CouponsController extends Controller
             'message' => 'Coupon successfully created.',
             'alert-type' => 'success'
         );
-        return redirect()->back()->with($notification);
+        return redirect()->route('admin.coupons.home')->with($notification);
     }
 
-    // Update coupon record
-    public  function update(Request $request, $id)
-    {
+    /*/ Update coupon record
+    public  function update(Request $request, $id){
         $this->validate($request, [
             'coupon' => 'required',
             'discount' => 'required|max:3'
@@ -50,26 +87,65 @@ class CouponsController extends Controller
             'alert-type' => 'success'
         );
         return redirect()->route('admin.coupons.index')->with($notification);
-    }
+    }*/
 
-    // Update coupon record
+
+    /**
+     * Update coupon record
+     *
+     * @param Request $request
+     * @param $id
+     * @return RedirectResponse
+     * @throws ValidationException
+     */
     public  function update(Request $request, $id)
     {
         $this->validate($request, [
-            'coupon' => 'required',
+            'code' => 'required',
             'discount' => 'required|max:3'
         ]);
-        $input = $request->all();
+        //dd($request);
         $coupon = Coupon::whereId($id)->first();
+
+        if ($request->has('statue')) {
+            $request->request->add(['statue' => 'active']);
+        } else {
+            $request->request->add(['statue' => 'inactive']);
+        }
+
+        $input = $request->all();
+
         $coupon->update($input);
         $notification = array(
             'message' => 'Coupon successfully updated.',
             'alert-type' => 'success'
         );
-        return redirect()->route('admin.coupons.index')->with($notification);
+        return redirect()->route('admin.coupons.home')->with($notification);
     }
 
-    // Apply Coupon
+    /**
+     * Delete coupon record
+     *
+     * @param $id
+     * @return RedirectResponse
+     */
+    public function delete($id)
+    {
+        Coupon::find($id)->delete();
+
+        $notification = array(
+            'message' => 'Coupon successfully deleted.',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('admin.coupons.home')->with($notification);
+    }
+
+    /**
+     * Apply coupon
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
     public function applyCoupon(Request $request)
     {
         $coupon = $request->coupon;
@@ -93,7 +169,12 @@ class CouponsController extends Controller
         }
     }
 
-    // Remove Coupon
+
+    /**
+     * Update coupon record
+     *
+     * @return RedirectResponse
+     */
     public function removeCoupon()
     {
         Session::forget('coupon');
