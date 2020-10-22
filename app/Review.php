99bba21dd41af11473b912ef89745c0600bd28fb
Youssef El Gharib <youssef.elgharib17@gmail.com>
2020-10-22 18:41:48 +0100

    Start set model function

--- app/Review.php
+++ app/Review.php
@@ -7,12 +7,5 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Review extends Model
 {
-<<<<<<< HEAD
-    public function workshop()
-    {
-        return $this->belongsTo('App\User');
-    }
-=======
     use SoftDeletes;
->>>>>>> 91eb42f92845b07a4d100dc65602150bdcd44042
 }
--- app/Workshop.php
+++ app/Workshop.php
@@ -7,17 +7,5 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Workshop extends Model
 {
-<<<<<<< HEAD
-    public function workshopCategory()
-    {
-        return $this->belongsTo('App\WorkshopCategory');
-    }
-
-    public function gallery()
-    {
-        return $this->hasMany('App\Gallery');
-    }
-=======
     use SoftDeletes;
->>>>>>> 91eb42f92845b07a4d100dc65602150bdcd44042
 }
