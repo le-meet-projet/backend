36780b4ab4faea89be611a0eb4719c25bc57e02d
Youssef El Gharib <youssef.elgharib17@gmail.com>
2020-10-22 19:01:50 +0100

    Fix Payment

--- app/Workshop.php
+++ app/Workshop.php
@@ -7,25 +7,10 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Workshop extends Model
 {
-<<<<<<< HEAD
- 
-=======
     use SoftDeletes;
 
->>>>>>> 34c4fa410578a37d6fb24db32486f4235ffae8b0
     public function workshopCategory()
     {
         return $this->belongsTo('App\WorkshopCategory');
     }
-<<<<<<< HEAD
-
-    public function gallery()
-    {
-        return $this->hasMany('App\Gallery');
-    }
- 
-    use SoftDeletes;
- 
-=======
->>>>>>> 34c4fa410578a37d6fb24db32486f4235ffae8b0
 }
