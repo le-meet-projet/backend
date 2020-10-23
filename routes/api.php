01664e494c69414cbf14f68250f2fad70d20bcae
Youssef El Gharib <youssef.elgharib17@gmail.com>
2020-10-23 20:53:39 +0100

    First api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/user', function (Request $request) {
+    return User::all()->toJson(JSON_PRETTY_PRINT);
 });
