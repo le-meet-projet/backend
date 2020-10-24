38fa2f9aa2e115f75d58c0258d4fc6be00504c13
Youssef El Gharib <youssef.elgharib17@gmail.com>
2020-10-25 00:17:56 +0100

    Add UserApiController function with tests

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -2,30 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserApiController extends Controller
 {
+    public function index()
+    {
+        return new JsonResponse(['User' => Auth::user()]);
+    }
+
     /**
      * VALIDATE REQUEST
      * CREATE
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function register()
+    public function register(Request $request)
     {
-        return null;
+        $validation = $request->validate([
+            'name' => 'required | string | min:4',
+            'email' => 'required | string',
+            'password' => 'required | string | min:8'
+        ]);
+        if ( !$validation ) {
+            return new JsonResponse([
+                'The data is not valid'
+            ]);
+        }
+        $data = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ];
+
+        User::create($data);
+        return new JsonResponse(['message' => 'You are registred']);
     }
 
     /**
      * VALIDATE REQUEST
      * UPDATE PROFILE & AVAtAR
+     *
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function updateInfo()
+    public function update(Request $request)
     {
-        return null;
+        $user = Auth::user();
+        $validation = $request->validate([
+            'name' => 'string',
+            'email' => 'string',
+            'password' => 'string'
+        ]);
+        if ( !$validation ) {
+            return new JsonResponse([
+                'The data is not valid'
+            ]);
+        }
+        $hashedPassword = $request->password ? Hash::make($request->password) : $user['password'];
+        $data = [
+            'name' => $request->name ?? $user['name'],
+            'email' => $request->email ?? $user['email'],
+            'password' => $hashedPassword,
+        ];
+
+        $user->update($data);
+        return new JsonResponse(['message' => 'You infos was updated with successfully !']);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,12 @@ use App\User;
 Route::group(['prefix' => 'v1'], function () {
     Route::group(['prefix' => 'user'], function () {
         Route::post('login', 'UserApiController@login');
-        Route::middleware(['auth:api'])->get('user', 'ApiController@user');
+        Route::post('register', 'UserApiController@register');
+        Route::middleware(['auth:api'])->post('update', 'UserApiController@update');
+        Route::middleware(['auth:api'])->get('', 'UserApiController@index');
+        Route::middleware(['auth:api'])->get('users', 'ApiController@user');
+        Route::middleware(['auth:api'])->get('favorites', 'ApiController@favorites');
     });
-    Route::get('favorites', 'ApiController@favorites');
     Route::get('orders', 'ApiController@orders');
     Route::get('workshops', 'ApiController@workshops');
     Route::get('search/{id}', 'ApiController@search');
