a5dedcedfd043a045511e42f453a92edd8fec1c4
faissaloux <fwahabali@gmail.com>
2020-11-19 13:23:09 +0100

    update: SpacesFactory

--- database/factories/SpacesFactory.php
+++ database/factories/SpacesFactory.php
@@ -12,7 +12,7 @@ $factory->define(Space::class, function (Faker $faker) {
 		'address' => $faker->address,
 		'description' => $faker->realText,
         'map' => $faker->sha256,
-		'type' => $faker->randomElement(['type1', 'type2']),
+		'type' => $faker->randomElement(['workshop', 'meeting']),
 		'date' => Carbon::now()->format('Y-m-d'),
 		'time' => $faker->time($format = 'H:i:s', $max = 'now'),
 		'capacity' => $faker->numberBetween($min = 10, $max = 100)
