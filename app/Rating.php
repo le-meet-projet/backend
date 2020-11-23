8b6dd579229315f4b5764d7fbf89c89f125badcc
Youssef El Gharib <youssef.elgharib17@gmail.com>
2020-11-23 21:30:32 +0100

    Add more logic to Rating class

--- app/Rating.php
+++ app/Rating.php
@@ -10,4 +10,14 @@ class Rating extends Model
     use SoftDeletes;
 
     protected $table = "rating";
+
+    public function spaces()
+    {
+        return $this->belongsTo('App\Space');
+    }
+
+    public function bestValue($r1, $r2)
+    {
+        return $r1->value > $r2->value;
+    }
 }
