442096d1d8edbcb6f635c0f6d0da1c0ac15bcd9a
faissaloux <fwahabali@gmail.com>
2021-05-26 19:34:51 +0200

    remove outdated api version

--- routes/api.php
+++ routes/api.php
@@ -2,109 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::group(['middleware' => ['cors', 'json.response']], function () {
-    Route::group(['prefix' => '/v2'], function () {
-        // USER
-        Route::post('register', 'Auth\ApiAuthController@register')->name('user.register');
-        Route::post('login', 'Auth\ApiAuthController@login')->name('user.login');
-        Route::post('otp', 'ApiController@otpConfirm')->name('otpConfirmation');
-        Route::middleware('auth:api')->group(function () {
-            Route::get('/logout', 'Auth\ApiAuthController@logout')->name('user.logout');
-        });
-        // SPACES
-        Route::group(['prefix' => 'spaces'], function () {
-            Route::get('/', 'ApiController@spaces')->name('spaces.all');
-            Route::middleware('auth:api')->group(function () {
-                // CREATE EDIT DELETE UPDATE SPACE
-                Route::post('/create', 'ApiController@createSpace')->name('spaces.create');
-                Route::get('/edit/{id}', 'ApiController@edit')->name('spaces.edit');
-                Route::post('/update/{id}', 'ApiController@updateSpace')->name('spaces.update');
-                Route::post('/delete/{id}', 'ApiController@deleteSpace')->name('spaces.delete');
-                // ADD OR REMOVE FROM FAVORITE
-                Route::post('/favorite/{id}', 'ApiController@addFavorite')->name('spaces.favorite.add');
-                Route::post('/favorite/{id}/delete', 'ApiController@deleteFavorite')->name('spaces.favorite.delete');
-                // ADD REVIEW
-                Route::post('/review/{id}', 'ApiController@addReview')->name('spaces.review.add');
-                // INVITATION
-                Route::group(['prefix' => 'invitation'], function () {
-                    Route::post('{id}', 'ApiController@inviteUserToSpace')->name('spaces.invite.user');
-                    Route::get('{id}/edit', 'ApiController@editInvitation')->name('spaces.invite.edit');
-                    Route::post('{id}/update', 'ApiController@updateInvitation')->name('spaces.invite.update');
-                    Route::post('/{id}/delete', 'ApiController@deleteInvitation')->name('spaces.invite.delete');
-                    Route::post('/{id}/answer', 'ApiController@acceptOrDenyInvitation')->name('spaces.invite.respond');
-                });
-            });
-            // ORDERS
-            Route::get('/{id}/order/details', 'ApiController@spaceOrderDetails')->name('space.orders.details');
-            // MEETING
-            Route::group(['prefix' => '/meeting'], function () {
-                Route::get('/', 'ApiController@getMeetingSpaces')->name('spaces.meeting.all');
-                Route::post('/sort', 'ApiController@sortMeetingSpaces')->name('spaces.meeting.sort');
-                Route::post('/search', 'ApiController@searchMeetingSpaces')->name('spaces.meeting.search');
-                Route::get('/{id}', 'ApiController@getMeetingId')->name('spaces.meeting.get');
-                Route::get('/{id}/reviews', 'ApiController@getMeetingReviews')->name('spaces.meeting.reviews');
-            });
-            // WORKSHOPS
-            Route::group(['prefix' => 'workshop'], function () {
-                Route::get('/', 'ApiController@getWorkShopsSpaces')->name('spaces.workshops.all');
-                Route::post('/sort', 'ApiController@sortWorkShops')->name('spaces.workshops.sort');
-                Route::post('/search', 'ApiController@searchWorkShops')->name('spaces.workshops.search');
-                Route::get('/{id}', 'ApiController@getWorkshopId')->name('spaces.workshops.get');
-                Route::get('/{id}/reviews', 'ApiController@getWorkshopReviews')->name('spaces.workshops.reviews');
-            });
-            // OFFICE
-            Route::group(['prefix' => 'office'], function () {
-                Route::get('/', 'ApiController@getOfficesSpaces')->name('spaces.offices.all');
-                Route::post('/sort', 'ApiController@sortOffices')->name('spaces.offices.sort');
-                Route::post('/search', 'ApiController@searchOffices')->name('spaces.offices.search');
-                Route::get('/{id}', 'ApiController@getOfficeId')->name('spaces.offices.get');
-                Route::get('/{id}/reviews', 'ApiController@getOfficeReviews')->name('spaces.offices.reviews');
-            });
-            // HOLIDAY
-            Route::group(['prefix' => 'holiday'], function () {
-                Route::get('/', 'ApiController@getHolidaysSpaces')->name('space.holidays.all');
-                Route::post('/sort', 'ApiController@sortHolidays')->name('space.holidays.all');
-                Route::post('/search', 'ApiController@searchHolidays')->name('space.holidays.all');
-                Route::get('/{id}', 'ApiController@getHolidayId')->name('space.holiday.all');
-                Route::get('/{id}/reviews', 'ApiController@getHolidayReviews')->name('space.holiday.all');
-            });
-        });
-
-        // ORDERS
-        Route::group(['prefix' => 'order'], function () {
-            Route::middleware('auth:api')->post('/{id}', 'ApiController@orderSpace');
-            Route::get('/{id}/details', 'ApiController@orderDetails');
-            Route::middleware('auth:api')->post('/payment/{id}', 'ApiController@PayOrder');
-        });
-
-        // USER
-        Route::group(['middleware' => 'auth:api', 'prefix' => 'user/'], function () {
-            Route::get('/profile', 'ApiController@profileUser')->name('user.profile.api');
-            Route::get('/edit', 'ApiController@editUser')->name('user.edit.api');
-            Route::post('/update', 'ApiController@updateUser')->name('user.update.api');
-            Route::post('/update/avatar', 'ApiController@updateAvatar')->name('user.update.avatar.api');
-            Route::post('/delete/{id}', 'ApiController@deleteUser')->name('user.delete.api');
-            Route::get('/{id}/ads', 'ApiController@userAds')->name('user.ads.api');
-            Route::get('/{id}/notifications', 'ApiController@userNotification')->name('user.notifications.api');
-            Route::get('/orders', 'ApiController@userOrders')->name('user.orders.api');
-            Route::get('/notification', 'ApiController@currentUserNotifications')->name('user.notifications.api');
-        });
-    });
-});
-
-
 Route::group(['middleware' => ['cors', 'json.response']], function () {
 
     Route::group(['prefix' => '/v3'], function () {
@@ -119,7 +16,7 @@ Route::group(['middleware' => ['cors', 'json.response']], function () {
         Route::post('/filter/vacation', '\App\Filter\VacationFilter@init');
         Route::post('/filter/workshop', '\App\Filter\WorkshopFilter@init');
         /// new api start
-        Route::post('/order/dates', 'ApiController@t');
+        Route::post('/order/dates', 'ApiController@order_dates');
         Route::post('/coupon/verify', 'ApiController@verify_coupon');
         Route::post('/coupon/verification', 'ApiController@verify_coupon');
         Route::post('/pay/secure', 'ApiController@pay_secure');
